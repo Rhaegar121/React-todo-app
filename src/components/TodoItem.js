f455aaf66a635ab3ecd462fd28f4b2b88b4fc023
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-02-19 17:31:34 +0630

    add handlechange function in the todoslogic.js

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,8 +1,27 @@
-const TodoItem = ({ itemProp }) => {
+const TodoItem = ({ itemProp, setTodos }) => {
+
+    const handleChange = (id) => {
+        setTodos((prevState) =>
+          prevState.map((todo) => {
+            if (todo.id === id) {
+              return {
+                ...todo,
+                completed: !todo.completed,
+              };
+            }
+            return todo;
+          })
+        );
+      };
+      
     return (
         <li>
-        <input type="checkbox" />
-        {itemProp.title}
+            <input
+            type="checkbox"
+            checked={itemProp.completed}
+            onChange={() => handleChange(itemProp.id)}
+            />
+            {itemProp.title}
         </li>
     )
   };
