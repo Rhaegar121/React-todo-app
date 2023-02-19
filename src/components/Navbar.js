b873bec5b37ea51e2d1f76c781fb73a11b5395d1
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-02-19 19:31:20 +0630

    add hamburger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,34 @@
-import { useState, useRef } from "react";
+import React, { useState, useRef } from "react";
 import { useOnClickOutside } from "../useOnClickOutside";
 
 const Navbar = () => {
-    const [dropdown, setDropdown] = useState(false);
-    const ref = useRef();
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  const [dropdown, setDropdown] = useState(false);
+  const ref = useRef();
 
-    useOnClickOutside(ref, dropdown, () => setDropdown(false));
+  useOnClickOutside(ref, dropdown, () => setDropdown(false));
 
-    return (
-        <nav>
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>
-            <button onClick={() => setDropdown(!dropdown)}>
-                Services <span>&#8595;</span>
-              </button>
-              {dropdown && (
-              <ul>
-                <li>Design</li>
-                <li>Development</li>
-              </ul>
-              )}
-            </li>
-          </ul>
-        </nav>
-      );
+  return (
+    <nav className="navbar">
+      <button className="toggle" onClick={() => setNavbarOpen((prev) => !prev)}>
+        {navbarOpen ? "close" : "open"}
+      </button>
+      <ul className={`menu-nav${navbarOpen ? ' show-menu' : ''}`}>
+        <li>Home</li>
+        <li>About</li>
+        <li>
+          <button onClick={() => setDropdown(!dropdown)}>
+            Services <span>&#8595;</span>
+          </button>
+          {dropdown && (
+            <ul>
+              <li>Design</li>
+              <li>Development</li>
+            </ul>
+          )}
+        </li>
+      </ul>
+    </nav>
+  );
 };
 export default Navbar;
