f4950b5ce54f7df465f82eade0640c590d68d27c
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-02-19 18:03:04 +0630

    add header.module.css

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,12 @@
 const Header = () => {
+    const headerStyle = {
+        padding: '20px 0',
+        lineHeight: '1.5em',
+        color: '#aeadad',
+        textAlign: 'center',
+      };
     return (
-      <header>
+        <header style={headerStyle}>
         <h1>todos</h1>
         <p>Items will persist in the browser local storage</p>
       </header>
