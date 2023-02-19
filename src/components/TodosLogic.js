116bc14d20b05c2cba8281a07a0de076c9a3c0e9
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-02-19 17:02:13 +0630

    update format document

--- src/components/TodosLogic.js
+++ src/components/TodosLogic.js
@@ -1,34 +1,33 @@
 import InputTodo from "./InputTodo";
 
 const TodosLogic = () => {
-    const todos = [
-      {
-        id: 1,
-        title: 'Setup development environment',
-        completed: true,
-      },
-      {
-        id: 2,
-        title: 'Develop website and add content',
-        completed: false,
-      },
-      {
-        id: 3,
-        title: 'Deploy to live server',
-        completed: false,
-      },
-    ];
-    return (
-        <div>
-            <InputTodo />
-        <ul>
+  const todos = [
+    {
+      id: 1,
+      title: "Setup development environment",
+      completed: true,
+    },
+    {
+      id: 2,
+      title: "Develop website and add content",
+      completed: false,
+    },
+    {
+      id: 3,
+      title: "Deploy to live server",
+      completed: false,
+    },
+  ];
+  return (
+    <div>
+      <InputTodo />
+      <ul>
         {todos.map((todo) => (
           <li>{todo.title}</li>
         ))}
       </ul>
-        </div>
-    );
-  };
-  
+    </div>
+  );
+};
+
 export default TodosLogic;
-  
