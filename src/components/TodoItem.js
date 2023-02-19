81efae5a1e7701e2cea886e193e554eaa9214dc1
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-02-19 18:36:16 +0630

    add setupdate function

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,29 +1,44 @@
-import styles from '../styles/TodoItem.module.css';
+import styles from "../styles/TodoItem.module.css";
+import { useState } from "react";
 
-const TodoItem = ({ itemProp, handleChange, delTodo }) => {
-    const completedStyle = {
-        fontStyle: 'italic',
-        color: '#595959',
-        opacity: 0.4,
-        textDecoration: 'line-through',
-      };
+const TodoItem = ({ itemProp, handleChange, delTodo, setUpdate }) => {
+  const [editing, setEditing] = useState(false);
 
-    return (
-        <li className={styles.item}>
-           <div className={styles.content}>
-            <input
-            type="checkbox"
-            checked={itemProp.completed}
-            onChange={() => handleChange(itemProp.id)}
-            />
-            <span style={itemProp.completed ? completedStyle : null}>
-        {itemProp.title}
-            </span>
-            <button>Edit</button>
-            <button onClick={() => delTodo(itemProp.id)}>Delete</button>
-            </div>
-        </li>
-    )
+  let viewMode = {};
+  let editMode = {};
+  if (editing) {
+    viewMode.display = "none";
+  } else {
+    editMode.display = "none";
+  }
+
+  const handleEditing = () => {
+    setEditing(true);
   };
-  export default TodoItem;
-  
+
+  const completedStyle = {
+    fontStyle: "italic",
+    color: "#595959",
+    opacity: 0.4,
+    textDecoration: "line-through",
+  };
+
+  return (
+    <li className={styles.item}>
+      <div className={styles.content} style={viewMode}>
+        <input
+          type="checkbox"
+          checked={itemProp.completed}
+          onChange={() => handleChange(itemProp.id)}
+        />
+        <span style={itemProp.completed ? completedStyle : null}>
+          {itemProp.title}
+        </span>
+        <button onClick={handleEditing}>Edit</button>
+        <button onClick={() => delTodo(itemProp.id)}>Delete</button>
+      </div>
+      <input type="text" value={itemProp.title} className={styles.textInput} style={editMode} onChange={(e) => setUpdate(e.target.value, itemProp.id)} />
+    </li>
+  );
+};
+export default TodoItem;
