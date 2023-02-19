20511d09300fc9cc139336b169384f4b19e10427
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-02-19 17:01:01 +0630

    render inputtodo in todoslogic.js

--- src/components/TodosLogic.js
+++ src/components/TodosLogic.js
@@ -1,3 +1,5 @@
+import InputTodo from "./InputTodo";
+
 const TodosLogic = () => {
     const todos = [
       {
@@ -17,11 +19,14 @@ const TodosLogic = () => {
       },
     ];
     return (
+        <div>
+            <InputTodo />
         <ul>
         {todos.map((todo) => (
           <li>{todo.title}</li>
         ))}
       </ul>
+        </div>
     );
   };
   
