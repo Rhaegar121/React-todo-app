2c1bba403bd08d1cced3d52f1d139763530401d4
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-02-19 18:45:25 +0630

    add navbar.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import TodoApp from './App';
+import Navbar from './components/Navbar';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Navbar />
+    <TodoApp />
   </React.StrictMode>
 );
 
