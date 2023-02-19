2ace0399a855f1a35feb0898f60b391ff7b201d5
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-02-19 17:41:22 +0630

    add handlesubmit function in inputtodo.js

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,13 +1,20 @@
 import { useState } from 'react';
-const InputTodo = () => {
+
+const InputTodo = ({ addTodoItem }) => {
   const [title, setTitle] = useState('');
 
   const handleChange = (e) => {
     setTitle(e.target.value);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTodoItem(title);
+    setTitle('');
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input
         type="text"
         placeholder="Add Todo..."
