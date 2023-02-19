25f43132c58bb5fdb2f7bb8e90580d99472821ac
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-02-19 16:50:41 +0630

    render todoslogic in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/header';
+import TodosLogic from './components/TodosLogic';
 
 const TodoApp = () => {
   return (
     <>
       <Header />
+      <TodosLogic />
     </>
   );
 };
