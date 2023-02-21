38859d92ac9534304f6764becb3b5b77ed72a4b2
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-02-21 17:50:30 +0630

    remove hard coded todo list

--- src/components/TodosLogic.js
+++ src/components/TodosLogic.js
@@ -5,23 +5,7 @@ import InputTodo from './InputTodo';
 import TodosList from './TodoList';
 
 const TodosLogic = () => {
-  const [todos, setTodos] = useState([
-    {
-      id: uuidv4(),
-      title: 'Setup development environment',
-      completed: true,
-    },
-    {
-      id: uuidv4(),
-      title: 'Develop website and add content',
-      completed: false,
-    },
-    {
-      id: uuidv4(),
-      title: 'Deploy to live server',
-      completed: false,
-    },
-  ]);
+  const [todos, setTodos] = useState([]);
 
   const handleChange = (id) => {
     setTodos((prevState) => prevState.map((todo) => {
